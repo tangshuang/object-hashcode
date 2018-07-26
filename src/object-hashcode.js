c8a091c97ef4f7e0ee2b9116cb09de77586f2237
frustigor <frustigor@gmail.com>
2018-07-26 20:52:28 +0800

    update

--- src/object-hashcode.js
+++ src/object-hashcode.js
@@ -1,6 +1,7 @@
 import getObjectHashcode from './getObjectHashcode'
 import defineObjectHashcodeProperty from './defineObjectHashcodeProperty'
 import createHashcodeObject from './createHashcodeObject'
+import { getStringHashcode } from './utils'
 
-export { getObjectHashcode, defineObjectHashcodeProperty, createHashcodeObject }
+export { getObjectHashcode, defineObjectHashcodeProperty, createHashcodeObject, getStringHashcode }
 export default getObjectHashcode
